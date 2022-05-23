eb7e881afa017c7adc0f7d27b05362682247dab2
An Dang <zyzo@users.noreply.github.com>
2022-05-23 22:39:15 +0200

    feat: working placeBet, deploy script (#1)

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,8 @@
 require("@nomiclabs/hardhat-waffle");
 
 const ALCHEMY_API_KEY = "Sgz6bc2NPV1oA4JLnmgBLEmiRVmlWaxq";
-const ROPSTEN_PRIVATE_KEY = "efece52df42960b6796df1dcf655550daaf19247b9464bc2b0ba4cd2c4e5fdfb";
-
+const ROPSTEN_PRIVATE_KEY =
+  "efece52df42960b6796df1dcf655550daaf19247b9464bc2b0ba4cd2c4e5fdfb";
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -19,7 +19,13 @@ module.exports = {
   networks: {
     ropsten: {
       url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
-      accounts: [`${ROPSTEN_PRIVATE_KEY}`]
-    }
-  }
+      accounts: [`${ROPSTEN_PRIVATE_KEY}`],
+    },
+    kovan: {
+      url: "https://eth-kovan.alchemyapi.io/v2/2ZKTg8koyehGPbox6erkvZ6GhjtED5P3",
+      accounts: [
+        "efece52df42960b6796df1dcf655550daaf19247b9464bc2b0ba4cd2c4e5fdfb",
+      ],
+    },
+  },
 };
